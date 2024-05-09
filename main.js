6ef776332c30f5c6d6cc8bb4271a26deb3fc9d47
aditDM <adhitnrahmat@gmail.com>
2024-05-09 23:44:36 +0800

    first

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
+// ini agar id data di myform saat di tekan submitnya akan di tampilkan di id output
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    // Validasi email
+    // Validasi email digunakan agar dapat pemberitahuan jika ada kesalahan di email sebagai contoh kekurangan @
     var email = document.getElementById("email").value;
     if (!validateEmail(email)) {
       alert("Please enter a valid email address.");
       return false;
     }
   
-    // Validasi nomor telepon
+    // Validasi nomor telepon jika nomornya tidak valik akan ada alert (pemberitahuan) sebagai contoh hanya 3 digit dimasukkan tapi harusnya 10 digit
     var phone = document.getElementById("phone").value;
     if (!validatePhone(phone)) {
       alert("Please enter a valid phone number.");
       return false;
     }
-  
+    // ini agar data yang di input di form akan di tampilkan di id output
     var formData = new FormData(this);
     var output = "<h2>Form Data:</h2><ul>";
     for (const entry of formData.entries()) {
@@ -22,6 +23,7 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     }
     output += "</ul>";
   
+    // ini agar foto yang di upload akan di tampilkan di id output
     var photo = document.getElementById("photo").files[0];
     var reader = new FileReader();
     reader.onload = function(e) {
@@ -30,14 +32,17 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     }
     reader.readAsDataURL(photo);
   
+    // ini agar formnya akan di reset setelah di submit
     this.reset();
   });
   
+  // ini fungsi validasi email
   function validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
   }
   
+  // ini fungsi validasi nomor telepon
   function validatePhone(phone) {
     var re = /^\d{10,20}$/;
     return re.test(phone);
